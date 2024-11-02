db3f72bee8bdcbc4389a8c61072490c32fce8aa9
sakibMERN <sakib.sr57@gmail.com>
2024-11-02 21:15:17 +0600

    set up createBrowserRouter and routeProvider and make a Root.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Root from './components/Root/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <RouterProvider router={router}></RouterProvider>
       
       
     </>
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h1>Hello root</h1>
+        </div>
+    );
+};
+
+export default Root;

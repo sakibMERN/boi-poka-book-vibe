32a6cd974cd0cd16187f03ffb5b6a8b86ac9a75d
sakibMERN <sakib.sr57@gmail.com>
2024-11-02 23:36:00 +0600

    update css inside Banner.jsx component and add json data inside public folder

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,17 @@
-import bannerImg from '../../assets/books.jpg'
+import bannerImg from "../../assets/book.png";
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={bannerImg}
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <div className="bg-[#131313] bg-opacity-5 lg:h-[554px] py-20 px-[120px] rounded-3xl flex flex-col lg:flex-row-reverse lg:justify-between lg:items-center">
+      
+        <img src={bannerImg} className="w-[318px] lg:-m-8" />
+        <div className="lg:w-[526px] space-y-12">
+          <h1 className="playfair-font text-[56px] font-bold">
+            Books to freshen up your bookshelf
+          </h1>
+          
+          <button className="btn bg-[#23BE0A] text-white">View The List</button>
         </div>
-      </div>
+      
     </div>
   );
 };
